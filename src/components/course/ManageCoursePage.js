9f9d40a85f8d13988e0d8cc7b7da0bf94726baec
Marcin Oziemski <oziemski.marcin@gmail.com>
2017-09-09 15:08:07 +0200

    test commit

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -40,6 +40,7 @@ class ManageCoursePage extends React.Component {
                 this.setState({saving: false});
             });
     }
+    
     redirect() {
         this.setState({saving: false});
         toastr.success('Course saved');
